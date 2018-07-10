3199bf6b0a771e4939a06433ace9346e47be495a
dwy555 <wenyudong0@gmail.com>
2018-07-10 16:49:23 -0700

    Update P031.java

--- P031.java
+++ P031.java
@@ -2,30 +2,27 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int len = nums.length;
-        int temp;
-        for(int i = len-1; i > 0; i--){
-            if(nums[i] > nums[i-1]){
-                int index = i;
-                for(int j = i; j < len; j++){
-                    if(nums[j] > nums[i-1] && nums[j] <= nums[index])index = j;
-                }
-                temp = nums[index];
-                nums[index] = nums[i-1];
-                nums[i-1] = temp;
-                
-                for(int m = i, n = len -1; m < n; m++, n-- ){
-                    temp = nums[n];
-                    nums[n] = nums[m];
-                    nums[m] = temp;
-                }   
-                return;
+        int temp,i;
+        
+        for(i = len-1; i > 0; i--)
+            if(nums[i] > nums[i-1])
+                break;
+        
+        if(i > 0){
+            int index = i;
+            for(int j = i; j < len; j++){
+                if(nums[j] > nums[i-1] && nums[j] <= nums[index])index = j;
             }
+            temp = nums[index];
+            nums[index] = nums[i-1];
+            nums[i-1] = temp;
         }
         
-        for(int m = 0, n = len -1; m < n; m++, n-- ){
+        for(int m = i, n = len -1; m < n; m++, n-- ){
             temp = nums[n];
             nums[n] = nums[m];
             nums[m] = temp;
-        }  
+        }   
+        
     }
 }
