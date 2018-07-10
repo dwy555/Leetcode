c3db5c6a9fc16e839d58a363a5ce06fef283aec3
dwy555 <wenyudong0@gmail.com>
2018-07-10 16:43:14 -0700

    Create P031.java

---
+++ P031.java
@@ -0,0 +1,31 @@
+// No.31 Next Permutation
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int len = nums.length;
+        int temp;
+        for(int i = len-1; i > 0; i--){
+            if(nums[i] > nums[i-1]){
+                int index = i;
+                for(int j = i; j < len; j++){
+                    if(nums[j] > nums[i-1] && nums[j] <= nums[index])index = j;
+                }
+                temp = nums[index];
+                nums[index] = nums[i-1];
+                nums[i-1] = temp;
+                
+                for(int m = i, n = len -1; m < n; m++, n-- ){
+                    temp = nums[n];
+                    nums[n] = nums[m];
+                    nums[m] = temp;
+                }   
+                return;
+            }
+        }
+        
+        for(int m = 0, n = len -1; m < n; m++, n-- ){
+            temp = nums[n];
+            nums[n] = nums[m];
+            nums[m] = temp;
+        }  
+    }
+}
