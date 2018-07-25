90d92c686d08b696c7aadb5e3d1848fa2a446d4b
dwy555 <wenyudong0@gmail.com>
2018-07-25 07:49:29 -0700

    Create P174.java

---
+++ P174.java
@@ -0,0 +1,21 @@
+// No.174 Dungeon Game
+class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        if(dungeon == null || dungeon.length == 0) return 0;
+        int m = dungeon.length, n = dungeon[0].length;
+        int dp[][] = new int[m][n];
+        for(int i = m - 1; i >= 0; i --){
+            for(int j = n - 1; j >= 0; j --){
+                if(i == m - 1 && j == n - 1) 
+                    dp[i][j] = Math.max(1, 1 - dungeon[i][j]);
+                else if(i == m - 1) 
+                    dp[i][j] = Math.max(1, dp[i][j + 1] - dungeon[i][j]);
+                else if(j == n - 1) 
+                    dp[i][j] = Math.max(1, dp[i + 1][j] - dungeon[i][j]);
+                else 
+                    dp[i][j] = Math.max(1, Math.min(dp[i][j + 1], dp[i + 1][j]) - dungeon[i][j]);
+            }
+        }
+        return dp[0][0];
+    }
+}
