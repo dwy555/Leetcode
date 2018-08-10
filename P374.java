75ca4a1e39d9b58435632ef97fdb03ad36591ae7
dwy555 <wenyudong0@gmail.com>
2018-08-10 02:08:13 -0700

    Create P374.java

---
+++ P374.java
@@ -0,0 +1,20 @@
+// No.374. Guess Number Higher or Lower
+/* The guess API is defined in the parent class GuessGame.
+   @param num, your guess
+   @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+      int guess(int num); */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int left = 1, right = n;
+        while(left <= right){
+            int mid = left+(right-left)/2;
+            int g = guess(mid);
+            if(g == 0)return mid;
+            if(g == 1) left = mid + 1;
+            else right = mid - 1;
+        }
+        return left;
+    }
+    
+}
