2465c1e7e1f8b970d1b47bc81195eb2c5e2c2a55
dwy555 <wenyudong0@gmail.com>
2018-08-26 00:13:55 -0700

    Create P122.java

---
+++ P122.java
@@ -0,0 +1,11 @@
+//No.122. Best Time to Buy and Sell Stock II
+class Solution {
+    public int maxProfit(int[] prices) {
+        int sum = 0;
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] > prices[i-1])
+                sum += prices[i]-prices[i-1];
+        }
+        return sum;
+    }
+}
