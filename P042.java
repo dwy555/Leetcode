ced145074745c4ca9c6e3864b5120268ad04bcfe
dwy555 <wenyudong0@gmail.com>
2018-08-27 02:04:12 -0700

    Update P042.java

--- P042.java
+++ P042.java
@@ -1,20 +1,27 @@
-// No.42 Trapping Rain Water
 class Solution {
     public int trap(int[] height) {
         int len = height.length;
         if(len == 0)return 0;
-        int count = 0;
-        int[] left_max = new int[len], right_max = new int[len];
-        left_max[0] = height[0];
-        right_max[len-1] = height[len-1];
-        for(int i = 1; i < len; i ++)
-            left_max[i] = Math.max(left_max[i-1], height[i]);
-        for(int i = len -2; i > -1; i --)
-            right_max[i] = Math.max(right_max[i+1], height[i]);
-        
-        for(int i = 0; i < len; i++)
-            count += Math.min(left_max[i], right_max[i])-height[i];
-        return count;
-        
+        int left = 0, right = len-1;
+        int left_max = 0, right_max = 0;
+        int ans = 0;
+        while(left < right){
+            if(height[left] < height[right]){
+                if(height[left] >= left_max){
+                    left_max = height[left];
+                }else{
+                    ans += left_max - height[left];
+                }
+                left++;
+            }else{
+                if(height[right] >= right_max){
+                    right_max = height[right];
+                }else{
+                    ans += right_max - height[right];
+                }
+                right--;
+            }
+        }
+        return ans;
     }
 }
