8b7b5df9cd074facf80f0647cf971835bb3555ed
dwy555 <wenyudong0@gmail.com>
2018-08-29 03:00:27 -0700

    Update P010.java

--- P010.java
+++ P010.java
@@ -1,27 +1,11 @@
 // No.Regular Expression Matching
 class Solution {
     public boolean isMatch(String s, String p) {
-        int sLen = s.length(), pLen = p.length();
-        boolean[][] dp = new boolean[sLen+1][pLen+1];
-        dp[0][0] = true;
-        for(int i = 1; i <= pLen; i++){
-            if(p.charAt(i-1) == '*')
-                dp[0][i] = dp[0][i-2];
-        }
-        for(int i = 0; i < sLen; i ++){
-            for(int j = 0; j < pLen; j++){
-                if(p.charAt(j) == '.' || p.charAt(j) == s.charAt(i))
-                    dp[i+1][j+1] = dp[i][j];
-                else if(p.charAt(j) == '*'){
-                    if(p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.')
-                        dp[i+1][j+1] = dp[i+1][j-1];
-                    else
-                        dp[i+1][j+1] = dp[i][j+1] || dp[i+1][j] || dp[i+1][j-1];
-                }
-                else dp[i+1][j+1] = false;
-            }
-        }
-        return dp[sLen][pLen];
-        
+        if(p.length() == 0)return s.length() == 0;
+        boolean first = s.length() > 0 && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0));
+        if(p.length() >= 2 && p.charAt(1) == '*')
+            return isMatch(s,p.substring(2)) || (first && isMatch(s.substring(1), p));
+        else
+            return first && isMatch(s.substring(1), p.substring(1));
     }
 }
