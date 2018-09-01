0c749ba557b87c0071ca9a99ea42f1d4aba599f2
dwy555 <wenyudong0@gmail.com>
2018-09-01 03:11:17 -0700

    Update P123.java

--- P123.java
+++ P123.java
@@ -2,21 +2,20 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int len = prices.length;
+        if(len <= 1)return 0;
         int[] left = new int[len];
-        int min = Integer.MAX_VALUE;
-        int max = 0;
-        for(int i = 0; i < len; i++){
-            max = Math.max(max,prices[i]-min);
-            min = Math.min(min, prices[i]);
-            left[i] = max;
-        }
-        max = 0;
-        int right = 0;
+        left[0] = 0;
+        int min = prices[0], max = prices[len-1];
         int ans = 0;
-        for(int i = len - 1; i >= 0; i--){
-            right = Math.max(right,max-prices[i]);
+        for(int i = 1; i < len; i++){
+            left[i] = Math.max(left[i-1],prices[i]-min);
+            min = Math.min(min, prices[i]);
+        }
+        int right = 0;
+        for(int i = len - 2; i >= 0; i--){
+            right = Math.max(right, max-prices[i]);
             max = Math.max(max, prices[i]);
-            ans = Math.max(ans, right+left[i]);
+            ans = Math.max(ans, left[i]+right);
         }
         return ans;
     }
