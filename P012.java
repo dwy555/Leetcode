4d6b20a3d134c4399bcac04c31c021a39149409c
dwy555 <wenyudong0@gmail.com>
2018-09-03 03:11:54 -0700

    Update P012.java

--- P012.java
+++ P012.java
@@ -4,13 +4,11 @@ class Solution {
         String[] roman = {"M", "CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         int[] nums = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
         StringBuilder sb = new StringBuilder();
-        int start = 0;
-        while(num != 0){
-            for(int i = start; i < nums.length; i ++){
+        while(num > 0){
+            for(int i = 0; i < nums.length; i++){
                 if(num >= nums[i]){
                     num -= nums[i];
                     sb.append(roman[i]);
-                    start = i;
                     break;
                 }
             }
