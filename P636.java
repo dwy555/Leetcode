461d3549bf6140426e2077e025b5bc3380f95843
dwy555 <wenyudong0@gmail.com>
2018-09-03 03:13:30 -0700

    Update P636.java

--- P636.java
+++ P636.java
@@ -1,19 +1,18 @@
 // No.636. Exclusive Time of Functions
 class Solution {
     public int[] exclusiveTime(int n, List<String> logs) {
-        
         int[] ans = new int[n];
-        Stack<Integer> st = new Stack<>();
+        Stack<Integer> s = new Stack<>();
         int prev = 0;
         for(String str: logs){
             String[] strs = str.split(":");
             if(strs[1].equals("start")){
-                if(!st.isEmpty())
-                    ans[st.peek()] += Integer.parseInt(strs[2]) - prev;
-                st.push(Integer.parseInt(strs[0]));
+                if(!s.isEmpty())
+                    ans[s.peek()] += Integer.parseInt(strs[2]) - prev;
+                s.push(Integer.parseInt(strs[0]));
                 prev = Integer.parseInt(strs[2]);
             }else{
-                int index = st.pop();
+                int index = s.pop();
                 ans[index] += Integer.parseInt(strs[2])+1 - prev;
                 prev = Integer.parseInt(strs[2])+1;
             }
