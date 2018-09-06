7652981d2055b6a87d0b8c8c0498a2f8e2739bf8
dwy555 <wenyudong0@gmail.com>
2018-09-06 00:43:56 -0700

    Update P215.java

--- P215.java
+++ P215.java
@@ -1,12 +1,12 @@
 // No.215. Kth Largest Element in an Array
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> q = new PriorityQueue();
-        for(Integer num: nums){
-            q.add(num);
-            if(q.size() > k)
-                q.poll();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int num: nums){
+            pq.add(num);
+            if(pq.size() > k)
+                pq.poll();
         }
-        return q.peek();
+        return pq.peek();
     }
 }
